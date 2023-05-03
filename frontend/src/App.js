f2d3b654b44de532722c67f6ca4f301abb2fa09d
pritammimo <crpritamsaha@gmail.com>
2023-05-03 22:46:58 +0530

    add

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 // page & layout imports
 import Homepage from './pages/Homepage'
 import ReviewDetails from './pages/ReviewDetails'
 import Category from './pages/Category'
 import SiteHeader from "./components/SiteHeader"
-
+const client = new ApolloClient({
+  uri: 'http://localhost:1337/graphql',
+  cache: new InMemoryCache()
+})
 function App() {
   return (
     <Router>
+    <ApolloProvider client={client}>
       <div className="App">
         <SiteHeader />
         <Switch>
@@ -23,7 +27,8 @@ function App() {
           </Route>
         </Switch>
       </div>
-    </Router>
+    </ApolloProvider>
+  </Router>
   );
 }
 
