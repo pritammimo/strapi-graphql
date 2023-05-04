72d015234f0375310c52df644c19d2fc630fe93b
pritammimo <crpritamsaha@gmail.com>
2023-05-04 22:57:56 +0530

    merge

--- frontend/src/components/SiteHeader.js
+++ frontend/src/components/SiteHeader.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useQuery, gql } from '@apollo/client'
+
+const CATEGORIES = gql`
+  query GetCategories {
+    categories{
+    data{
+      id
+      attributes{
+        name
+      }
+    }
+  }
+  }
+`
 
 export default function SiteHeader() {
+  const { loading, error, data } = useQuery(CATEGORIES)
+
+  if (loading) return <p>Loading categories...</p>
+  if (error) return <p>Error fetching categories</p>
+   console.log("site",data);
   return (
     <div className="site-header">
       <Link to="/"><h1>Ninja Reviews</h1></Link>
+      <nav className="categories">
+        <span>Filter reviews by category:</span>
+        {data.categories.data.map(category => (
+          <Link key={category.id} to={`/category/${category.id}`}>
+            {category.attributes.name}
+          </Link>
+        ))}
+      </nav>
     </div>
   )
 }
